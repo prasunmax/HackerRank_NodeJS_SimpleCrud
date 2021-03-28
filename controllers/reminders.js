ce04b7aea24f2f9913af90f5e1f306c950ec3f44
prasunmax <prasunmax@gmail.com>
2021-03-28 10:37:56 +0530

    Fixed all issues

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -1,7 +1,74 @@
 const Reminders = require('../models/reminders');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
+
 
 
 module.exports = {
-
+    post_api: post_handler,
+    get_api: get_handler
 }
+
+
+function post_handler(req, res, next) {
+    Reminders.create(req.body)
+        .then(data => {
+            res.status(201).send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Reminder."
+            });
+        });
+};
+
+function get_handler(req, res, next) {
+    const id = req.params.id;
+    if (id) {
+        Reminders.findByPk(id)
+            .then(data => {
+                if (data)
+                    res.status(200).send(data);
+                else
+                    res.status(404).send("ID not found");
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "ID not found"
+                });
+            });
+    } else {
+        const user = req.query.user;
+        const after = req.query.after;
+        var hasqry = false;
+        
+        var jsonData = {};
+        var whereJson = {};
+        
+        if (user) {
+            jsonData['user'] = user;
+            hasqry = true;
+        }
+        if (after) {
+            var dateJson = {};
+            dateJson[Op.gte] = parseInt(after);
+            jsonData['date'] = dateJson;
+            hasqry = true;
+        }
+        if (hasqry) {
+            //qryStr = '{ "where": { ' + qryStr + ' } }';
+            whereJson['where'] = jsonData;
+        } else {
+            qryStr = '{}';
+        }
+        Reminders.findAll(whereJson)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "No Records Found"
+                });
+            });        
+    }
+};
+
--- routes/reminders.js
+++ routes/reminders.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 const controller = require('../controllers/reminders');
 
+router.get('/', controller.get_api);
+router.get('/:id', controller.get_api);
+router.post('/', controller.post_api);
+router.patch('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.put('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.delete('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
 module.exports = router;
